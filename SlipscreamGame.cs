5003d041d05898d19ac9fade9310faca89d1d79c
Rodolfo Tedeschi <rodolfo.tedeschi@gmail.com>
2022-05-10 21:47:04 +0200

    road test

--- SlipscreamGame.cs
+++ SlipscreamGame.cs
@@ -1,32 +1,45 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace Slipscream
 {
 	public class SlipscreamGame : Game
 	{
-		private Texture2D? car;
-		private Vector2 position;
+		private int bendFactor;
+		private int position;
+		private Texture2D? road;
 		private SpriteBatch? spriteBatch;
 
-		public SlipscreamGame() => _ = new GraphicsDeviceManager(this);
+		int Height => GraphicsDevice.PresentationParameters.BackBufferHeight;
+
+		public SlipscreamGame()
+		{
+			_ = new GraphicsDeviceManager(this);
+			IsFixedTimeStep = false;
+		}
 
 		protected override void Draw(GameTime gameTime)
 		{
 			GraphicsDevice.Clear(Color.Black);
+			spriteBatch!.Begin(blendState: BlendState.NonPremultiplied);
 
-			spriteBatch!.Begin();
-			spriteBatch.Draw(car, position, Color.White);
+			for (int z = 0; z < Height; z++)
+			{
+				int y = Height - z - 1;
+				int x = z / 2 + (int) (Math.Sqrt(1 - Math.Pow((float) z / Height, 2)) * bendFactor) + 100 - bendFactor;
+				spriteBatch.Draw(road, new Rectangle(x, y, road!.Width - z, 1), new Rectangle(0, (y - position + Height) % Height, road.Width, 1), Color.White);
+			}
+
 			spriteBatch.End();
-
 			base.Draw(gameTime);
 		}
-
+		
 		protected override void LoadContent()
 		{
 			spriteBatch = new SpriteBatch(GraphicsDevice);
-			car = Texture2D.FromFile(GraphicsDevice, "Car.png");
+			road = Texture2D.FromFile(GraphicsDevice, "Road.png");
 		}
 
 		protected override void Update(GameTime gameTime)
@@ -34,9 +47,8 @@ namespace Slipscream
 			if (Keyboard.GetState().IsKeyDown(Keys.Escape))
 				Exit();
 
-			position.X = (int) gameTime.TotalGameTime.TotalMilliseconds % GraphicsDevice.PresentationParameters.BackBufferWidth;
-			position.Y = (int) gameTime.TotalGameTime.TotalMilliseconds % GraphicsDevice.PresentationParameters.BackBufferHeight;
-
+			bendFactor = (int) (Math.Sin((int) gameTime.TotalGameTime.TotalMilliseconds % 2_000 / 2_000.0 * Math.PI * 2) * 300);
+			position = (int) gameTime.TotalGameTime.TotalMilliseconds % Height;
 			base.Update(gameTime);
 		}
 	}
